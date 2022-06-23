be8905e91c9644c7c4f1ff18d89c45015bb5699f
cyrus01337 <cyrus21337@gmail.com>
2022-06-23 01:45:16 +0100

    Flesh out core functionality

--- Set.lua
+++ Set.lua
@@ -2,35 +2,138 @@ local Set = {}
       Set.__index = Set
 
 
-function Set.new(...)
-    local self = {
-        Mapping = {}
-    }
+function Set.new(...: any)
+    local self = setmetatable({}, Set)
+          self.Ordered = {}
+          self.Mapping = {}
 
-    for _, v in ipairs({...}) do
-        self:Add(v)
+          self.Pop = self.Discard
+          self.Remove = self.Discard
+
+    for _, value in ipairs({...}) do
+        self:Add(value)
     end
 
-    return setmetatable(self, Set)
+    return self
 end
 
 
-function Set:Add(element)
-    self.Mapping[element] = self.Mapping[element] or element
+function Set:__index(key)
+    return Set[key] or self.Ordered[key]
 end
 
 
-function Set:Remove(element)
-    self.Mapping[element] = nil
+function Set:Add(value)
+    if self.Mapping[value] then return end
+
+    self.Mapping[value] = {
+        value
+    }
+
+    table.insert(self.Ordered, value)
 end
 
 
-function Set:Pop(element)
-    local ret = self.Mapping[element]
+function Set:Clear()
+    self.Mapping = {}
+    self.Ordered = {}
+end
 
-    self:Remove(element)
 
-    return ret
+function Set:Copy()
+    return Set.new(table.unpack(self.Ordered))
+end
+
+
+function Set:Difference(...)
+    local difference = {}
+
+    for _, set in ipairs({...}) do
+        for key, _ in ipairs(set.Ordered) do
+            if self.Mapping[key] then continue end
+
+            table.insert(difference, key)
+        end
+    end
+
+    return Set.new(table.unpack(difference))
+end
+
+
+function Set:Discard(value)
+    if not self.Mapping[value] then return end
+
+    self.Mapping[value] = nil
+
+    return table.remove(self.Ordered, table.find(self.Ordered, value))
+end
+
+
+function Set:Intersection(...)
+    local intersection = {}
+
+    for key, _ in ipairs(self.Ordered) do
+        for _, set in ipairs({...}) do
+            if not set[key] then continue end
+
+            table.insert(intersection, key)
+        end
+    end
+
+    return Set.new(table.unpack(intersection))
+end
+
+
+function Set:IsADisjoint(set)
+    for key, _ in ipairs(set.Ordered) do
+        if self.Mapping[key] then
+            return true
+        end
+    end
+
+    return false
+end
+
+
+function Set:IsASubset(set)
+    for key, _ in ipairs(self.Ordered) do
+        if not set[key] then
+            return false
+        end
+    end
+
+    return true
+end
+
+
+function Set:IsASuperset(set)
+    for key, _ in ipairs(set.Ordered) do
+        if not self.Mapping[key] then return false end
+    end
+
+    return true
+end
+
+
+function Set:Union(...)
+    local union = {}
+
+    for _, set in ipairs({...}) do
+        for key, _ in ipairs(set.Ordered) do
+            table.insert(union, key)
+        end
+    end
+
+    return Set.new(table.unpack(union))
+end
+
+
+function Set:Update(...)
+    for _, set in ipairs({...}) do
+        for key, _ in ipairs(set.Ordered) do
+            self:Add(key)
+        end
+    end
 end
 
 
